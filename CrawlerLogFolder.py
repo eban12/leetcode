f240eed6c80396cfecb7efb1db35a6aecb93e02e
Eyob Alemu <it.eyob.alemu@gmail.com>
2023-02-07 00:39:41 +0300

    Create CrawlerLogFolder.py

---
+++ CrawlerLogFolder.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for log in logs:
+            if log == "../" and count > 0:
+                count -= 1
+            elif log not in {"../", "./"}:
+                print(log)
+                count += 1
+        return count
